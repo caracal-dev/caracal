2f982830cc4645e8603ac1ccf72b9ac39c3d9b06
tumillanino <anthony.tumia@live.com.au>
2026-05-06 16:42:40 +1000

    simplify theming

---
+++ system_files/shared/usr/share/plasma/shells/org.kde.plasma.desktop/contents/updates/caracal-pins.js
@@ -0,0 +1,40 @@
+const desiredLaunchers = [
+    "applications:app.zen_browser.zen.desktop",
+    "applications:org.alacritty.Alacritty.desktop",
+    "applications:io.github.kolunmi.Bazaar.desktop",
+    "applications:ardour9.desktop",
+    "preferred://filemanager"
+];
+
+const legacyLaunchers = [
+    "applications:Alacritty.desktop,applications:app.zen_browser.zen.desktop,applications:org.kde.dolphin.desktop,applications:io.github.kolunmi.Bazaar.desktop",
+    "applications:org.alacritty.Alacritty.desktop,applications:io.github.kolunmi.Bazaar.desktop,applications:app.zen_browser.zen.desktop,applications:ardour9.desktop"
+];
+
+const allPanels = panels();
+
+for (let i = 0; i < allPanels.length; ++i) {
+    const panel = allPanels[i];
+    panel.floating = false;
+
+    const widgets = panel.widgets();
+    for (let j = 0; j < widgets.length; ++j) {
+        const widget = widgets[j];
+
+        if (widget.type === "org.kde.plasma.kickoff") {
+            widget.currentConfigGroup = ["General"];
+            widget.writeConfig("icon", "distributor-logo");
+            widget.reloadConfig();
+        }
+
+        if (widget.type === "org.kde.plasma.icontasks") {
+            widget.currentConfigGroup = ["General"];
+            const currentLaunchers = widget.readConfig("launchers", "");
+
+            if (!currentLaunchers || legacyLaunchers.indexOf(currentLaunchers.trim()) !== -1) {
+                widget.writeConfig("launchers", desiredLaunchers);
+                widget.reloadConfig();
+            }
+        }
+    }
+}
