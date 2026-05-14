4fe82ce10a535da7d14ffdc4212120de49dc33e8
tumillanino <anthony.tumia@live.com.au>
2026-05-15 01:00:27 +1000

    issue with previous terra replacement

--- build_files/build.sh
+++ build_files/build.sh
@@ -29,8 +29,8 @@ dnf5 -y copr enable tumillanino/caracal-packages
 
 dnf -y install --nogpgcheck --repofrompath 'terra,https://repos.fyralabs.com/terra$releasever' terra-release
 
-dnf -y install 'https://mirrors.rpmfusion.org/nonfree/fedora/rpmfusion-nonfree-release-$(rpm -E %fedora).noarch.rpm'
-dnf -y install 'https://mirrors.rpmfusion.org/free/fedora/rpmfusion-free-release-$(rpm -E %fedora).noarch.rpm'
+dnf -y install "https://mirrors.rpmfusion.org/nonfree/fedora/rpmfusion-nonfree-release-$(rpm -E %fedora).noarch.rpm"
+dnf -y install "https://mirrors.rpmfusion.org/free/fedora/rpmfusion-free-release-$(rpm -E %fedora).noarch.rpm"
 
 dnf5 -y install caracal-setup caracal-software-installer caracal-audio-controller
 
