dd7573da77d5386205f4a0988fd766012cc8904c
tumillanino <anthony.tumia@live.com.au>
2026-09-22 10:16:13 +1000

    added some new wallpapers/ new KDE default wp

--- system_files/kinoite/usr/share/plasma/look-and-feel/org.kde.breezedark.desktop/contents/plasmoidsetupscripts/org.kde.plasma.folder.js
+++ system_files/kinoite/usr/share/plasma/look-and-feel/org.kde.breezedark.desktop/contents/plasmoidsetupscripts/org.kde.plasma.folder.js
@@ -2,6 +2,6 @@
 
 applet.wallpaperPlugin = "org.kde.image"
 applet.currentConfigGroup = ["Wallpaper", "org.kde.image", "General"]
-applet.writeConfig("Image", "file:///usr/share/wallpapers/caracal/caracal-lake.png")
-applet.writeConfig("PreviewImage", "file:///usr/share/wallpapers/caracal/caracal-lake.png")
+applet.writeConfig("Image", "file:///usr/share/wallpapers/caracal/caracal-mist.png")
+applet.writeConfig("PreviewImage", "file:///usr/share/wallpapers/caracal/caracal-mist.png")
 applet.reloadConfig()
